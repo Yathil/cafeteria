b6d7b83927d0de4411be00a6d0a9a07041226533
Yathil <92367275+Yathil@users.noreply.github.com>
2024-03-19 16:51:48 +0100

    Creación de estructura de carpetas y primeras clases

---
+++ lib/services/database_helper.dart
@@ -0,0 +1,1 @@
+// la lógica para interactuar con la base de datos SQLite, como la inicialización de la base de datos, la creación de tablas, consultas, inserciones, actualizaciones, eliminaciones, etc.
